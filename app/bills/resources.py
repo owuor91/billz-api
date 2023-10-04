951c9677a474890f199065285a450fcb2d874c7a
John Owuor <owuor91@gmail.com>
2023-10-04 16:13:51 +0300

    post or update upcoming bill

--- app/bills/resources.py
+++ app/bills/resources.py
@@ -1,6 +1,9 @@
 from app.bills.schemas import BillSchema, UpcomingBillSchema
 from app.bills.model import Bill, UpcomingBill
 from app.base.resource import BaseResource
+from flask import request
+from db import db
+from flask_jwt_extended import jwt_required
 class BillResource(BaseResource):
     model = Bill
     schema = BillSchema()
@@ -9,3 +12,14 @@ class BillResource(BaseResource):
 class UpcomingBillResource(BaseResource):
     model = UpcomingBill
     schema = UpcomingBillSchema()
+
+    @jwt_required()
+    def post(self):
+        session = db.session
+        data = request.get_json()
+        upcoming = UpcomingBill(**data)
+        existing = session.query(UpcomingBill).get(upcoming.upcoming_bill_id)
+        if existing is None:
+            return super().post()
+        else:
+            return super().put(upcoming.upcoming_bill_id)
